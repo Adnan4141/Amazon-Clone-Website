145585c64cfac0ffe5e2df85c21f78b7d833fd27
Adnanh4141 <adnanh4141@gmail.com>
2024-03-22 03:06:04 +0600

    favicon added

--- src/msg/Value.jsx
+++ src/msg/Value.jsx
@@ -2,9 +2,9 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { useDispatch } from 'react-redux';
 import { deleteTodos } from '../redux/TodosSlics';
 
-const Value = ({id,todo,category}) => {
+const Value = ({id,todo}) => {
     const dispatch = useDispatch();
-    
+
   return (
   <div className="my-2 flex justify-between w-full border border-l-[6px] border-TodolightGreen p-2   ">
       <p className="text-white font-bold ">
